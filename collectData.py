333e0c1ce1796d749f320a0364ead54b68efe19b
Musili Andrew <andrewthepyrookie@gmail.com>
2025-01-21 08:17:53 +0300

    files upload

--- collectData.py
+++ collectData.py
@@ -1,1 +1,11 @@
-print("hello")
+class Circle:
+    pi = 3.142
+    
+    def __init__(self, radius):
+        self.radius = radius
+        
+    @classmethod
+    def calculate_circumference(cls, radius):
+        return 2* cls.pi * radius
+    
+print(Circle.calculate_circumference(5))
